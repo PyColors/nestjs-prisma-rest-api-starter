40dc2825d1e5584ccf2617fc429b651e5fa0138b
P@trice <patweb44@gmail.com>
2023-04-04 19:06:00 +0200

    Integrated validation using the ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Automatically remove all non-whitelisted properties from DTO
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const config = new DocumentBuilder()
     .setTitle('NestJs Prisma REST API Starter')
